9ff916b6bc6dc497df38b6dc8c85368e853f3945
Cory Vander Jagt <coryvj@github.com>
2016-01-30 13:21:57 -0800

    Update deploy

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -2,6 +2,6 @@ cd /Users/coryvj/exports/decktape
 ./export.sh
 cd /Users/coryvj/github/presentations/resources
 bundle exec middleman build
-cd /Users/coryvj/heroku/gh-remark/
+cd /Users/coryvj/heroku/gh-remark
 ./deploy.sh
 cd /Users/coryvj/github/presentations/resources
